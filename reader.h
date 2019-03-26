43916e2b8eb004b1553757075932ee21c31237f6
Jansen <jansen-avila@hotmail.com>
2019-03-25 22:40:16 -0300

    Cabeçalho das Funcoes

---
+++ reader.h
@@ -0,0 +1,17 @@
+// reader
+
+void preenche_vet(int *vet,int n);
+int *vet_preenche(int n);
+void imprime(int *vetor,int n);
+void limpa(int *vetor); 
+int **preenche_matriz(int l,int c);
+
+
+
+
+
+
+
+
+
+
