6ae5f23fe00e7277e602f87c8b5ceea0ccfe592a
Jansen <jansen-avila@hotmail.com>
2019-04-02 16:53:57 -0300

    Agenda v4

--- agenda.c
+++ agenda.c
@@ -3,76 +3,93 @@
 #include <math.h>
 
 typedef struct inf{
-    char nome[40];
+    int nome[40];
     //int data,mes,ano;
     int cpf,telefone;
 
 }pessoas;
 
-// Pelo jeito, meu pBuffer só vai servir para armazenar minha agenda, mas não irei utiliza-lo para fazer
-// as instruções das funções. Ele só irá ser a minha referencia.
-
 void *pBuffer;
-pessoas *ptr;
+pessoas *ptr,*ptr_aux;
 
 
-void adicionar(pessoas* p, int* c,int* lacos){
-	
+void adicionar(pessoas* p,pessoas *ptr_aux, int* c,int *lacos){
+
+    if((*c) == 1) ptr_aux = (pessoas*) malloc (sizeof(pessoas));
+    else ptr_aux = (pessoas*) realloc (ptr_aux,(*c)*sizeof(pessoas));
+
 	printf("Diga o seu nome: ");
-	scanf("%s", p->nome);
-	printf("Diga o seu CPF: ");
-	scanf("%d", &p->cpf);
-	printf("Diga o seu Telefone: ");
-	scanf("%d", &p->telefone);
-	
-//	p =  lacos+1;
-}
+	scanf(" %s", ptr_aux->nome);
+    printf("Diga seu CPF: ");
+    scanf("%d", &ptr_aux->cpf);
+    printf("Diga o seu Telefone: ");
+    scanf("%d", &ptr_aux->telefone);
 
 
-void listar(pessoas *p) {
-	
+    pBuffer = realloc(pBuffer,(3*sizeof(int)) + ((*c)*sizeof(pessoas)));
+
+    if((*c) == 1) {
+    ptr_aux = (pessoas*)(lacos + 1);
+    ptr = ptr_aux;
+    }
+    else {
+    ptr_aux = (pessoas*) ptr + 1;
+    }
+
+
+    printf("memoria ptr: %d\n", ptr);
+    printf("memoria ptr_aux: %d\n", ptr_aux);
+    }
+
+
+void listar(pessoas *p,pessoas *p_aux,int *lacos, int* c) {
+
+    for(*lacos = 0; *lacos <= *c; ++(*lacos)){
 	printf("Nome: %s\n", p->nome);
 	printf("CPF: %d\n", p->cpf);
 	printf("Telefone: %d\n", p->telefone);
-	
-}
+	p = p + sizeof(pessoas);
+    }
+
+    }
+
+
+
 
 
 int main() {
 
 	int *n,*cont,*lacos;
-	
-	n = (int*) malloc (sizeof(int));
-    cont = (int*) malloc (sizeof(int));
-    lacos = (int*) malloc (sizeof(int));
-    
+
     pBuffer = malloc (3*sizeof(int));
-    
+
     n = pBuffer;
     cont = n + 1;
     lacos = cont + 1;
 
-    
+    printf("n = %d\n",n);
+    printf("cont = %d\n",cont);
+    printf("lacos = %d\n", lacos);
+
     *cont = 0;
-    
-    
+
+
       do{
 
       printf("1) Adicionar\n2) Apagar\n3) Buscar\n4) Listar\n5) Sair\n");
       scanf("%d", &(*n));
-	
+
     switch(*n) {
 
      case 1:
-			printf("Adicionar\n");
+
 			*cont = *cont +1;
-			if(*cont == 1) ptr = (pessoas*) malloc ((*cont)*sizeof(pessoas));
-			else ptr = (pessoas*) realloc (ptr,(*cont)*sizeof(pessoas));
-			adicionar(ptr,cont,lacos);			
-			
-			// Quando retornar o ptr, tenho que adicionar ele no pBuffer, provalvemente fazer outro malloc.	
-			
-			
+            adicionar(ptr,ptr_aux,cont,lacos);
+
+
+
+
+
             break;
      case 2:
             printf("Apagar\n");
@@ -85,7 +102,7 @@ int main() {
             break;
      case 4:
             printf("Listar\n");
-            listar(ptr);
+            listar(ptr,ptr_aux,&lacos,cont);
             break;
 
      case 5: break;
